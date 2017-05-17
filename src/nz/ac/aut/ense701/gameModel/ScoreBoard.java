2366db85c5a4fb3eb3d8d9910c7398aa91687bea
Aashish <aashish.si@gmail.com>
2017-05-17 23:20:37 +1200

    ScoreBoard file Reading + Unit tests

--- src/nz/ac/aut/ense701/gameModel/ScoreBoard.java
+++ src/nz/ac/aut/ense701/gameModel/ScoreBoard.java
@@ -5,7 +5,16 @@
  */
 package nz.ac.aut.ense701.gameModel;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import nz.ac.aut.ense701.gameQuiz.QuizData;
+import org.apache.commons.io.FileUtils;
 
 /**
  *
@@ -13,15 +22,19 @@ import java.util.List;
  */
 public class ScoreBoard {
     private static final String SCORE_BOARD_FILE_NAME = "PlayerScore.json";
+    private static final String ENCODING = "UTF-8";
+    
     private static List<Score> scoreBoard;
     
-    public static List<Score> getScoreBoard(){
-        if(scoreBoard == null){
-        }
+    public static List<Score> getScoreBoard() throws IOException{
+        readFromFile();
         return scoreBoard;
     }
     
-    public void readFromFile(){
-        
+    public static void readFromFile() throws IOException{
+        if(scoreBoard == null){
+            Type targetClassType = new TypeToken<ArrayList<Score>>() {}.getType();
+            scoreBoard = (List<Score>) new Gson().fromJson( FileUtils.readFileToString(new File(SCORE_BOARD_FILE_NAME), ENCODING), targetClassType);
+        }
     }
 }
